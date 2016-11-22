a4e95233c92cae93f99f4020329679386fc5352d
LynnChen <lynnchen@jing-an-rd2.local>
2016-11-22 17:31:43 +0800

    renew js files

--- js/all.js
+++ js/all.js
@@ -1,39 +1,56 @@
-var startTime = 5*60000+1000;
-var duration = startTime;
+(function () {
 
-function Timer() {
+  //倒數5分鐘
+  var timeout = 5 * 60 * 1000;
 
-    duration = duration - 1000;
+  var temp = timeout;
 
-    var milliseconds = parseInt((duration%1000)/100)
-        , seconds = parseInt((duration/1000)%60)
-        , minutes = parseInt((duration/(1000*60))%60)
-        , hours = parseInt((duration/(1000*60*60))%24);
+  var btn_start = document.getElementById('btn_start');
+
+  var btn_stop = document.getElementById('btn_stop');
+
+  var btn_reset = document.getElementById('btn_reset');
+
+  // 顯示秒數
+  function updateText () {
+      el = document.getElementById('timer');
+      el.innerHTML = msToTime();
+      temp = temp - 1000;
+  }
+
+  // 秒數轉換時間格式
+  function msToTime() {
+    var milliseconds = parseInt((temp%1000)/1000)
+        , seconds = parseInt((temp/1000)%60)
+        , minutes = parseInt((temp/(1000*60))%60)
+        , hours = parseInt((temp/(1000*60*60))%24);
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    document.getElementById('timer').innerHTML = hours + ":" + minutes + ":" + seconds;
-}
+    var a = hours + ":" + minutes + ":" + seconds;
+    return a;
+  }
 
-// show start time
-Timer();
+  // 按鈕事件
+  btn_start.addEventListener('click', function () {
+    clearInterval(timer);
+    timer = setInterval(updateText, 1000);
+  })
 
-// reset start time
-function resetTimer() {
-  duration = startTime;
-  stopTimer();
-  Timer();
-};
+  btn_stop.addEventListener('click', function () {
+    if(timer){
+      clearInterval(timer);
+    }
+  })
 
-// start count down
-function startTimer() {
-  stopTimer();
-  timer = setInterval(Timer, 1000);
-};
-
-// pause count down
-function stopTimer() {
-  clearInterval(timer);
-};
+  btn_reset.addEventListener('click', function () {
+    temp = timeout;
+    clearInterval(timer);
+    updateText();
+  })
+  
+  // 呼叫顯示秒數
+  updateText();
+})();
